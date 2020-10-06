decfaf20463e426aa9d6241149c45d6ff316c20d
DESKTOP-6M43LSH\tqlon <tqlong1609@gmail.com>
2020-10-06 17:46:04 +0700

    Finish: add firebase api

--- android/app/src/main/java/com/datingapprn/MainApplication.java
+++ android/app/src/main/java/com/datingapprn/MainApplication.java
@@ -16,6 +16,7 @@ import java.util.List;
 import com.wix.interactable.Interactable;
 import com.airbnb.android.react.lottie.LottiePackage;
 import com.reactcommunity.rndatetimepicker.RNDateTimePickerPackage;
+// import io.invertase.firebase.app.ReactNativeFirebaseAppPackage;
 // import com.horcrux.svg.SvgPackage;
 
 public class MainApplication extends Application implements ReactApplication {
@@ -36,6 +37,7 @@ public class MainApplication extends Application implements ReactApplication {
           packages.add(new LottiePackage());
           packages.add(new Interactable()); 
           packages.add(new LinearGradientPackage()); 
+          // packages.add(new ReactNativeFirebaseAppPackage()); 
           // packages.add(new RNDateTimePickerPackage());
           // packages.add(new ExtraDimensionsPackage()); 
           // packages.add(new SvgPackage());
