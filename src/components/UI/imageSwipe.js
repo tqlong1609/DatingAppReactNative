5bed6afa1e7155035dd0763f11d9d5361e83b131
tqlong1609 <tqlong1609@gmail.com>
2020-10-11 15:48:39 +0700

    Finish: get position when swipe

--- src/components/UI/imageSwipe.js
+++ src/components/UI/imageSwipe.js
@@ -1,23 +1,88 @@
 import React from 'react';
 import { StyleSheet, Text, View, Dimensions, Image, Animated, PanResponder, TouchableOpacity }
     from 'react-native';
-
+import Themes from '/src/themes'
 const SCREEN_HEIGHT = Dimensions.get('window').height
 const SCREEN_WIDTH = Dimensions.get('window').width
 
-import Themes from '/src/themes'
-import Ionicons from 'react-native-vector-icons/Ionicons'
-const COLOR_ICON = 'white'
 let isSwipe = false
 
-const Users = [
-    { id: "1", uri: "https://raw.githubusercontent.com/nathvarun/React-Native-Layout-Tutorial-Series/master/Project%20Files/12%20Tinder%20Swipe%20Deck/%232%20Complete%20Animation/assets/1.jpg" },
-    { id: "2", uri: "https://raw.githubusercontent.com/nathvarun/React-Native-Layout-Tutorial-Series/master/Project%20Files/12%20Tinder%20Swipe%20Deck/%232%20Complete%20Animation/assets/2.jpg" },
-    { id: "3", uri: "https://raw.githubusercontent.com/nathvarun/React-Native-Layout-Tutorial-Series/master/Project%20Files/12%20Tinder%20Swipe%20Deck/%232%20Complete%20Animation/assets/3.jpg" },
-    { id: "4", uri: "https://raw.githubusercontent.com/nathvarun/React-Native-Layout-Tutorial-Series/master/Project%20Files/12%20Tinder%20Swipe%20Deck/%232%20Complete%20Animation/assets/4.jpg" },
-    { id: "5", uri: "https://raw.githubusercontent.com/nathvarun/React-Native-Layout-Tutorial-Series/master/Project%20Files/12%20Tinder%20Swipe%20Deck/%232%20Complete%20Animation/assets/5.jpg" },
+const dataImage = [
+    {
+        id: "1",
+        arrUri: [
+            "https://raw.githubusercontent.com/nathvarun/React-Native-Layout-Tutorial-Series/master/Project%20Files/12%20Tinder%20Swipe%20Deck/%232%20Complete%20Animation/assets/1.jpg",
+            "https://raw.githubusercontent.com/nathvarun/React-Native-Layout-Tutorial-Series/master/Project%20Files/12%20Tinder%20Swipe%20Deck/%232%20Complete%20Animation/assets/2.jpg",
+            "https://raw.githubusercontent.com/nathvarun/React-Native-Layout-Tutorial-Series/master/Project%20Files/12%20Tinder%20Swipe%20Deck/%232%20Complete%20Animation/assets/3.jpg",
+            "https://raw.githubusercontent.com/nathvarun/React-Native-Layout-Tutorial-Series/master/Project%20Files/12%20Tinder%20Swipe%20Deck/%232%20Complete%20Animation/assets/4.jpg",
+            "https://raw.githubusercontent.com/nathvarun/React-Native-Layout-Tutorial-Series/master/Project%20Files/12%20Tinder%20Swipe%20Deck/%232%20Complete%20Animation/assets/5.jpg"
+        ]
+    },
+    {
+        id: "2",
+        arrUri: [
+            "https://raw.githubusercontent.com/nathvarun/React-Native-Layout-Tutorial-Series/master/Project%20Files/12%20Tinder%20Swipe%20Deck/%232%20Complete%20Animation/assets/1.jpg",
+            "https://raw.githubusercontent.com/nathvarun/React-Native-Layout-Tutorial-Series/master/Project%20Files/12%20Tinder%20Swipe%20Deck/%232%20Complete%20Animation/assets/2.jpg",
+            "https://raw.githubusercontent.com/nathvarun/React-Native-Layout-Tutorial-Series/master/Project%20Files/12%20Tinder%20Swipe%20Deck/%232%20Complete%20Animation/assets/3.jpg",
+            "https://raw.githubusercontent.com/nathvarun/React-Native-Layout-Tutorial-Series/master/Project%20Files/12%20Tinder%20Swipe%20Deck/%232%20Complete%20Animation/assets/4.jpg",
+            "https://raw.githubusercontent.com/nathvarun/React-Native-Layout-Tutorial-Series/master/Project%20Files/12%20Tinder%20Swipe%20Deck/%232%20Complete%20Animation/assets/5.jpg"
+        ]
+    },
+    {
+        id: "3",
+        arrUri: [
+            "https://raw.githubusercontent.com/nathvarun/React-Native-Layout-Tutorial-Series/master/Project%20Files/12%20Tinder%20Swipe%20Deck/%232%20Complete%20Animation/assets/2.jpg",
+            "https://raw.githubusercontent.com/nathvarun/React-Native-Layout-Tutorial-Series/master/Project%20Files/12%20Tinder%20Swipe%20Deck/%232%20Complete%20Animation/assets/3.jpg",
+            "https://raw.githubusercontent.com/nathvarun/React-Native-Layout-Tutorial-Series/master/Project%20Files/12%20Tinder%20Swipe%20Deck/%232%20Complete%20Animation/assets/4.jpg",
+            "https://raw.githubusercontent.com/nathvarun/React-Native-Layout-Tutorial-Series/master/Project%20Files/12%20Tinder%20Swipe%20Deck/%232%20Complete%20Animation/assets/5.jpg"
+        ]
+    },
+    {
+        id: "4",
+        arrUri: [
+            "https://raw.githubusercontent.com/nathvarun/React-Native-Layout-Tutorial-Series/master/Project%20Files/12%20Tinder%20Swipe%20Deck/%232%20Complete%20Animation/assets/1.jpg",
+            "https://raw.githubusercontent.com/nathvarun/React-Native-Layout-Tutorial-Series/master/Project%20Files/12%20Tinder%20Swipe%20Deck/%232%20Complete%20Animation/assets/2.jpg",
+            "https://raw.githubusercontent.com/nathvarun/React-Native-Layout-Tutorial-Series/master/Project%20Files/12%20Tinder%20Swipe%20Deck/%232%20Complete%20Animation/assets/3.jpg",
+            "https://raw.githubusercontent.com/nathvarun/React-Native-Layout-Tutorial-Series/master/Project%20Files/12%20Tinder%20Swipe%20Deck/%232%20Complete%20Animation/assets/4.jpg",
+            "https://raw.githubusercontent.com/nathvarun/React-Native-Layout-Tutorial-Series/master/Project%20Files/12%20Tinder%20Swipe%20Deck/%232%20Complete%20Animation/assets/5.jpg"
+        ]
+    },
+    {
+        id: "5",
+        arrUri: [
+            "https://raw.githubusercontent.com/nathvarun/React-Native-Layout-Tutorial-Series/master/Project%20Files/12%20Tinder%20Swipe%20Deck/%232%20Complete%20Animation/assets/1.jpg",
+            "https://raw.githubusercontent.com/nathvarun/React-Native-Layout-Tutorial-Series/master/Project%20Files/12%20Tinder%20Swipe%20Deck/%232%20Complete%20Animation/assets/2.jpg",
+            "https://raw.githubusercontent.com/nathvarun/React-Native-Layout-Tutorial-Series/master/Project%20Files/12%20Tinder%20Swipe%20Deck/%232%20Complete%20Animation/assets/3.jpg",
+            "https://raw.githubusercontent.com/nathvarun/React-Native-Layout-Tutorial-Series/master/Project%20Files/12%20Tinder%20Swipe%20Deck/%232%20Complete%20Animation/assets/4.jpg",
+            "https://raw.githubusercontent.com/nathvarun/React-Native-Layout-Tutorial-Series/master/Project%20Files/12%20Tinder%20Swipe%20Deck/%232%20Complete%20Animation/assets/5.jpg"
+        ]
+    }
 ]
+const STATUS = {
+    LIKE: "Like",
+    UNLIKE: "Unlike"
+}
 
+const SIZE_IMAGE_PER = 95
+const TabSwipe = (props) => {
+    const { length, currentIndexPicture } = props
+    return (<View style={{
+        backgroundColor: Themes.Colors.GRAY_BRIGHT_I,
+        width: `${SIZE_IMAGE_PER}%`, height: 8, position: 'absolute', zIndex: 1,
+        alignSelf: 'center',
+        marginTop: 20,
+        borderRadius: 10,
+        flexDirection: 'row'
+    }}>
+        <View style={{
+            backgroundColor: Themes.Colors.GRAY_BRIGHT_IV,
+            width: `${(length - currentIndexPicture) * 100 / length}%`,
+            height: '100%',
+            borderRadius: 10,
+        }}>
+        </View>
+    </View>)
+}
 export default class ImageSwipe extends React.Component {
 
     constructor() {
@@ -25,7 +90,8 @@ export default class ImageSwipe extends React.Component {
 
         this.position = new Animated.ValueXY()
         this.state = {
-            currentIndex: 0
+            currentIndex: 0,
+            currentIndexPicture: 0
         }
 
         this.rotate = this.position.x.interpolate({
@@ -66,17 +132,45 @@ export default class ImageSwipe extends React.Component {
 
     }
 
+    backPicture = () => {
+        const { currentIndexPicture, currentIndex } = this.state
+        if (currentIndexPicture > 0) {
+            this.setState({ currentIndexPicture: currentIndexPicture - 1 })
+        }
+    }
+
+    nextPicture = () => {
+        const { currentIndexPicture, currentIndex } = this.state
+        const length = dataImage[currentIndex].arrUri.length
+        if (currentIndexPicture < length - 1) {
+            this.setState({ currentIndexPicture: currentIndexPicture + 1 })
+        }
+    }
+
     checkIsSwipe = (evt) => {
         if (isSwipe) { isSwipe = false }
         else {
             const { locationX } = evt.nativeEvent
             if (locationX < SCREEN_WIDTH / 2) {
-                console.log('left')
+                this.backPicture()
             } else {
-                console.log('right')
+                this.nextPicture()
             }
         }
     }
+
+
+    //TODO: handle when like/unlike
+    stateSwipe = (state) => {
+        this.setState({ currentIndexPicture: 0 })
+        switch (state) {
+            case STATUS.LIKE:
+                break;
+            case STATUS.UNLIKE:
+                break;
+        }
+    }
+
 
     UNSAFE_componentWillMount() {
         this.PanResponder = PanResponder.create({
@@ -89,6 +183,7 @@ export default class ImageSwipe extends React.Component {
             onPanResponderRelease: (evt, gestureState) => {
                 this.checkIsSwipe(evt)
                 if (gestureState.dx > 120) {
+                    this.stateSwipe(STATUS.LIKE)
                     Animated.spring(this.position, {
                         toValue: { x: SCREEN_WIDTH + 100, y: gestureState.dy },
                         useNativeDriver: true
@@ -99,6 +194,7 @@ export default class ImageSwipe extends React.Component {
                     })
                 }
                 else if (gestureState.dx < -120) {
+                    this.stateSwipe(STATUS.UNLIKE)
                     Animated.spring(this.position, {
                         toValue: { x: -SCREEN_WIDTH - 100, y: gestureState.dy },
                         useNativeDriver: true
@@ -118,22 +214,21 @@ export default class ImageSwipe extends React.Component {
             }
         })
     }
-
-    handlePress(evt) {
-        console.log('evt.nativeEvent.locationX', evt.nativeEvent.locationX)
-        // Alert.alert(`x coord = ${evt.nativeEvent.locationX}`);
-        // Alert.alert(`y coord = ${evt.nativeEvent.locationY}`);
-    }
-
     renderUsers = () => {
+        let length
+        if (this.state.currentIndex < dataImage.length) {
+            length = dataImage[this.state.currentIndex].arrUri.length
+        }
+        else {
+            console.log('Empty data')
+        }
+        return dataImage.map((item, index) => {
 
-        return Users.map((item, i) => {
 
-
-            if (i < this.state.currentIndex) {
+            if (index < this.state.currentIndex) {
                 return null
             }
-            else if (i == this.state.currentIndex) {
+            else if (index == this.state.currentIndex) {
 
                 return (
                     <Animated.View
@@ -148,12 +243,10 @@ export default class ImageSwipe extends React.Component {
                             <Text style={{ borderWidth: 1, borderColor: 'red', color: 'red', fontSize: 32, fontWeight: '800', padding: 10 }}>NOPE</Text>
 
                         </Animated.View>
-
+                        <TabSwipe length={length} currentIndexPicture={this.state.currentIndexPicture} />
                         <Image
                             style={{ height: '100%', width: '100%', resizeMode: 'cover', borderRadius: 20 }}
-                            source={{ uri: item.uri }} />
-
-
+                            source={{ uri: item.arrUri[this.state.currentIndexPicture] }} />
                     </Animated.View>
                 )
             }
@@ -172,12 +265,11 @@ export default class ImageSwipe extends React.Component {
 
                         <Animated.View style={{ opacity: 0, transform: [{ rotate: '30deg' }], position: 'absolute', top: 50, right: 40, zIndex: 1000 }}>
                             <Text style={{ borderWidth: 1, borderColor: 'red', color: 'red', fontSize: 32, fontWeight: '800', padding: 10 }}>NOPE</Text>
-
                         </Animated.View>
-
+                        <TabSwipe length={length} currentIndexPicture={0} />
                         <Image
                             style={{ height: '100%', width: '100%', resizeMode: 'cover', borderRadius: 20 }}
-                            source={{ uri: item.uri }} />
+                            source={{ uri: item.arrUri[0] }} />
 
                     </Animated.View>
                 )
