252d48ead890304e4259992f4e1f0d61e8b735a4
DESKTOP-6M43LSH\tqlon <tqlong1609@gmail.com>
2020-10-15 12:18:01 +0700

    Finish: add expandable view Todo: theme my Profile

--- src/components/MyProfile/myProfile.js
+++ src/components/MyProfile/myProfile.js
@@ -1,40 +1,97 @@
-import React from 'react'
-import { StyleSheet, Text, View, ScrollView, Image, TouchableOpacity } from 'react-native'
-import ButtonNext from '/src/components/UI/buttonNext'
+import React, { useState } from 'react';
+import {
+    Switch,
+    ScrollView,
+    StyleSheet,
+    Text,
+    View,
+    Image,
+    TouchableOpacity,
+} from 'react-native';
+// import Constants from 'expo-constants';
+import * as Animatable from 'react-native-animatable';
+import Collapsible from 'react-native-collapsible';
+import Accordion from 'react-native-collapsible/Accordion';
 import Themes from '/src/themes'
-import ButtonItemEdit from '/src/components/UI/buttonItemEdit'
-import { withTranslation } from 'react-i18next'
 import IconAwesome5 from 'react-native-vector-icons/FontAwesome5'
 
+const BACON_IPSUM =
+    'Bacon ipsum dolor amet chuck turducken landjaeger tongue spare ribs. Picanha beef prosciutto meatball turkey shoulder shank salami cupim doner jowl pork belly cow. Chicken shankle rump swine tail frankfurter meatloaf ground round flank ham hock tongue shank andouille boudin brisket. ';
 
-function ItemContent(props) {
-    const { title, content } = props
-    return (
-        <View>
-            <View style={{
-                backgroundColor: Themes.Colors.GRAY_BRIGHT_V,
-                height: HEIGHT_HEADER
-            }}>
-                <Text style={styles.txtHeader}>{title}</Text>
-            </View>
-            <View style={styles.txtContent}>
-                <Text style={{ color: '#939093', fontSize: 16 }}>{content}</Text>
-            </View>
-        </View>
-    )
-}
+const CONTENT = [
+    {
+        title: 'First',
+        content: BACON_IPSUM,
+    },
+    {
+        title: 'Second',
+        content: BACON_IPSUM,
+    },
+    {
+        title: 'Third',
+        content: BACON_IPSUM,
+    },
+    {
+        title: 'Fourth',
+        content: BACON_IPSUM,
+    },
+    {
+        title: 'Fifth',
+        content: BACON_IPSUM,
+    },
+];
 
-//TODO: About you
-function MyProfile(props) {
-    const { t } = props
-    const onPressIcon = () => {
-        console.log('ico')
+const SELECTORS = [
+    {
+        title: 'First',
+        value: 0,
+    },
+    {
+        title: 'Third',
+        value: 2,
+    },
+    {
+        title: 'None',
+    },
+];
+
+export default function MyProfile(props) {
+    const [activeSections, setActiveSections] = useState([])
+    const [collapsed, setCollapsed] = useState(true)
+    const [multipleSelect, setMultipleSelect] = useState(false)
+
+    const toggleExpanded = () => {
+        setCollapsed(!collapsed);
+    };
+
+    const setSections = sections => {
+        setActiveSections(sections.includes(undefined) ? [] : sections)
+        // this.setState({
+        //     activeSections: sections.includes(undefined) ? [] : sections,
+        // });
+    };
+
+    const renderHeader = (section, _, isActive) => {
+        return (
+            <Animatable.View
+                duration={400}
+                style={[styles.header, isActive ? styles.active : styles.inactive]}
+                transition="backgroundColor"
+            >
+                <Text style={styles.headerText}>{section.title}</Text>
+            </Animatable.View>
+        );
+    };
+
+    const renderContent = (section, _, isActive) => {
+        return (
+            <ItemContent title={"Name"} content={"Trần Long"} isActive={isActive} />
+        );
     }
+
     return (
-        <View style={{ flex: 1 }}>
-            <ScrollView
-                showsVerticalScrollIndicator={false}
-                style={styles.container}>
+        <View style={styles.container}>
+            <ScrollView contentContainerStyle={{ paddingTop: 30 }}>
                 <View style={styles.containerHeader}>
                     <View>
                         <Image
@@ -53,21 +110,53 @@ function MyProfile(props) {
                     <Text style={styles.txtInfo}>Long, 21</Text>
                     <Text style={styles.txtDetail}>Tqlong1609@gmail.com</Text>
                 </View>
-                <ItemContent title={"Name"} content={"Trần Long"} />
-                <ItemContent title={"Age"} content={"21"} />
-                <ItemContent title={"Gender"} content={"Male"} />
-                <ItemContent title={"Phone"} content={"+84966712391"} />
-                <ItemContent title={"Email"} content={"Tqlong1609@gmail.com"} />
-                <ItemContent title={"Location"} content={"Ho Chi Minh, VietNam"} />
-
+                <View style={styles.multipleToggle}>
+                    <Text style={styles.multipleToggle__title}>Multiple Select?</Text>
+                    <Switch
+                        value={multipleSelect}
+                        onValueChange={a => setMultipleSelect(a)}
+                    />
+                </View>
+                <Collapsible collapsed={collapsed} align="center">
+                    <View style={styles.content}>
+                        <Text>
+                            Bacon ipsum dolor amet chuck turducken landjaeger tongue spare
+                            ribs
+                        </Text>
+                    </View>
+                </Collapsible>
+                <Accordion
+                    activeSections={activeSections}
+                    sections={CONTENT}
+                    touchableComponent={TouchableOpacity}
+                    expandMultiple={multipleSelect}
+                    renderHeader={renderHeader}
+                    renderContent={renderContent}
+                    duration={400}
+                    onChange={setSections}
+                />
             </ScrollView>
-            <ButtonNext isGradient={true} isCheck={true} />
         </View>
-
-    )
+    );
 }
 
-export default withTranslation()(MyProfile)
+function ItemContent(props) {
+    const { title, content, isActive } = props
+
+    return (
+        <View>
+            <View style={{
+                backgroundColor: Themes.Colors.GRAY_BRIGHT_V,
+                height: HEIGHT_HEADER
+            }}>
+                <Text style={styles.txtHeader}>{title}</Text>
+            </View>
+            <View style={styles.txtContent}>
+                <Text style={{ color: '#939093', fontSize: 16 }}>{content}</Text>
+            </View>
+        </View>
+    )
+}
 
 const SIZE = 160
 const SIZE_ICON = 35
@@ -108,7 +197,6 @@ const styles = StyleSheet.create({
     },
     containerHeader: {
         alignItems: 'center',
-        marginTop: MARGIN_TOP
     },
     imgAvatar: {
         height: SIZE,
@@ -117,5 +205,62 @@ const styles = StyleSheet.create({
     },
     container: {
         flex: 1,
-    }
-})
+    },
+    container: {
+        flex: 1,
+        backgroundColor: '#F5FCFF',
+        paddingTop: 10,
+    },
+    title: {
+        textAlign: 'center',
+        fontSize: 22,
+        fontWeight: '300',
+        marginBottom: 20,
+    },
+    header: {
+        backgroundColor: '#F5FCFF',
+        padding: 10,
+    },
+    headerText: {
+        textAlign: 'center',
+        fontSize: 16,
+        fontWeight: '500',
+    },
+    content: {
+        padding: 20,
+        backgroundColor: '#fff',
+    },
+    active: {
+        backgroundColor: 'rgba(255,255,255,1)',
+    },
+    inactive: {
+        backgroundColor: 'rgba(245,252,255,1)',
+    },
+    selectors: {
+        marginBottom: 10,
+        flexDirection: 'row',
+        justifyContent: 'center',
+    },
+    selector: {
+        backgroundColor: '#F5FCFF',
+        padding: 10,
+    },
+    activeSelector: {
+        fontWeight: 'bold',
+    },
+    selectTitle: {
+        fontSize: 14,
+        fontWeight: '500',
+        padding: 10,
+    },
+    multipleToggle: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        marginVertical: 30,
+        alignItems: 'center',
+    },
+    multipleToggle__title: {
+        fontSize: 16,
+        marginRight: 8,
+    },
+});
