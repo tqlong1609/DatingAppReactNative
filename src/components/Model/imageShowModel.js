8c96781a2ca031738e4ddcb1c13e4a756487666d
DESKTOP-6M43LSH\tqlon <tqlong1609@gmail.com>
2020-10-17 11:41:27 +0700

    FINISH: spilit componet myAlbums

---
+++ src/components/Model/imageShowModel.js
@@ -0,0 +1,84 @@
+import React, { useState } from 'react'
+import { StyleSheet, TouchableOpacity, View } from 'react-native'
+import { Modal } from 'react-native';
+import ImageViewer from 'react-native-image-zoom-viewer';
+import Ionicons from 'react-native-vector-icons/Ionicons'
+import Themes from '/src/themes'
+
+export default function imageShowModel(props) {
+    const { visible, setIsVisible, images } = props
+    const [isShowMenu, setIsShowMenu] = useState(false)
+
+    const onClickImage = () => {
+        setIsShowMenu(!isShowMenu)
+    }
+
+    const onLongPressImage = () => {
+        isShowMenu && setIsShowMenu(false)
+    }
+
+    const onPressBack = () => {
+        setIsVisible(false)
+    }
+    return (
+        <Modal visible={visible} transparent={true}
+        >
+            {isShowMenu && <View style={styles.containerTabHeader}>
+                <TouchableOpacity
+                    onPress={() => onPressBack()}
+                >
+                    <Ionicons
+                        size={SIZE_ICON_HEADER}
+                        color={'white'}
+                        name="chevron-back-outline"></Ionicons>
+                </TouchableOpacity>
+                <TouchableOpacity >
+                    <Ionicons
+                        size={SIZE_ICON_HEADER}
+                        color={'white'}
+                        name="reorder-three-outline"></Ionicons>
+                </TouchableOpacity>
+            </View>}
+            <ImageViewer imageUrls={images}
+                onClick={() => onClickImage()}
+                onLongPress={() => onLongPressImage()}
+                onSave={(index) => console.log(index)}
+            />
+            {isShowMenu && <View style={styles.containerTabFooter}>
+                <TouchableOpacity>
+                    <Ionicons
+                        size={SIZE_ICON_FOOTER}
+                        color={'white'}
+                        name="pencil-outline"></Ionicons>
+                </TouchableOpacity>
+                <TouchableOpacity>
+                    <Ionicons
+                        size={SIZE_ICON_FOOTER}
+                        color={'white'}
+                        name="share-social-outline"></Ionicons>
+                </TouchableOpacity>
+                <TouchableOpacity >
+                    <Ionicons
+                        size={SIZE_ICON_FOOTER}
+                        color={'white'}
+                        name="trash-outline"></Ionicons>
+                </TouchableOpacity>
+            </View>}
+        </Modal>
+    )
+}
+
+const SIZE_ICON_FOOTER = 25
+const SIZE_ICON_HEADER = 30
+const styles = StyleSheet.create({
+    containerTabHeader: {
+        ...Themes.Styles.TabMenu,
+        justifyContent: 'space-between',
+
+    },
+    containerTabFooter: {
+        ...Themes.Styles.TabMenu,
+        justifyContent: 'space-around',
+        bottom: 0
+    }
+})
