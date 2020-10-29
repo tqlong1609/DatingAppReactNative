7af64d516e372977f964105814cc28311abe773d
DESKTOP-6M43LSH\tqlon <tqlong1609@gmail.com>
2020-10-29 09:19:41 +0700

    Finish: add api signup email

---
+++ src/api/url.js
@@ -0,0 +1,3 @@
+const URL_NETWORK = "http://192.168.1.120:5000"
+
+export const URL_SIGN_UP_EMAIL = URL_NETWORK + "/api/account/register"
--- src/components/SignUpEmail/signUpEmail.Controller.js
+++ src/components/SignUpEmail/signUpEmail.Controller.js
@@ -2,10 +2,10 @@ import React from 'react'
 import SignUpEmail from './signUpEmail'
 import { useNavigation } from '@react-navigation/native'
 import Const from '/src/const'
+import Api from '/src/api'
 
 export default function SignUpEmailController() {
     const navigation = useNavigation()
-
     const onSignUpEmail = (params) => {
         const { name, email, password } = params
         requestPostSignUpApi(name, email, email, password)
@@ -16,22 +16,15 @@ export default function SignUpEmailController() {
     }
 
     const requestPostSignUpApi = async (name, email, confirmEmail, password) => {
-        fetch('http://localhost:5000/api/account/register/', {
-            method: 'POST',
-            headers: {
-                Accept: 'application/json',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                email: email,
-                confirmEmail: confirmEmail,
-                password: password,
-                name: name,
-                gender: "male",
-                dateOfBirth: "09/16/1999",
-                city: "HCM",
-                country: "Vietnam"
-            })
+        Api.RequestApi.postRequestApi(Api.Url.URL_SIGN_UP_EMAIL, {
+            email: email,
+            confirmEmail: confirmEmail,
+            password: password,
+            name: name,
+            gender: "male",
+            dateOfBirth: "09/16/1999",
+            city: "HCM",
+            country: "Vietnam"
         })
             .then((response) => response.json())
             .then((json) => console.log(json))
